5dba4677331956815f37f38413767741b603fc8a
DearYuto <reasons@kakao.com>
2025-05-30 16:32:09 +0900

    feat(Validation): 검증 타입 추가

---
+++ src/common/types/validation/Validation.ts
@@ -0,0 +1,7 @@
+export interface Validation {
+  minLength?: { value: number; message?: string };
+  maxLength?: { value: number; message?: string };
+  min?: { value: number; message?: string };
+  max?: { value: number; message?: string };
+  pattern?: { value: RegExp; message?: string };
+}
