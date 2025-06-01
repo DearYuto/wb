3fc2433f53eff1efffd58409270696eaeb6e754d
DearYuto <reasons@kakao.com>
2025-06-01 19:41:29 +0900

    refactor(CreateProduct): 상품 생성 페이지 중복 로직 제거, zod 어댑터 사용한 resolver 세팅

--- src/app/products/new/page.tsx
+++ src/app/products/new/page.tsx
@@ -1,18 +1,24 @@
 'use client';
 
 import { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, UseFormRegister } from 'react-hook-form';
 
 import { ProductType } from '@/domains/product/types/product';
 import * as Form from '@radix-ui/react-form';
-import { createProduct } from '@/domains/product/service/createProductService';
+import { createProduct } from '@/domains/product/services/createProductService';
+import { zodResolver } from '@hookform/resolvers/zod';
+import productFormSchema from '@/domains/product/form/validations/schemas/productFormSchema';
+import formFields from '@/domains/product/form/fields/formFields';
+import { ProductFormFieldType } from '@/domains/product/types/productFormField';
 
 const CreateProductPage = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<ProductType>();
+  } = useForm<ProductType>({
+    resolver: zodResolver(productFormSchema),
+  });
 
   const [price, setPrice] = useState(0);
   const [discountPercentage, setDiscountPercentage] = useState(0);
@@ -28,108 +34,13 @@ const CreateProductPage = () => {
     <section>
       <h2>Create Product</h2>
       <Form.Root onSubmit={handleSubmit(onSubmitForm)}>
-        <Form.Field name="title">
-          <Form.Label>Title</Form.Label>
-          <Form.Control asChild>
-            <input
-              {...register('title', {
-                required: '*required',
-                maxLength: { value: 15, message: '15자 이하로 입력해주세요' },
-              })}
-            />
-          </Form.Control>
-          {errors.title && <Form.Message>{errors.title.message}</Form.Message>}
-        </Form.Field>
-
-        <Form.Field name="description">
-          <Form.Label>Description</Form.Label>
-          <Form.Control asChild>
-            <textarea rows={4} />
-          </Form.Control>
-        </Form.Field>
-
-        <Form.Field name="price">
-          <Form.Label>Price</Form.Label>
-          <Form.Control
-            asChild
-            onChange={(e) => {
-              const value = e.target.value;
-
-              if (!value) {
-                setPrice(0);
-              }
-
-              if (Number.isNaN(Number(value))) {
-                return;
-              }
-
-              setPrice(Number(value));
-            }}
-          >
-            <input
-              type="number"
-              {...register('price', {
-                required: '*required',
-                min: { value: 1000, message: '1,000원 이상 입력해주세요' },
-              })}
-            />
-          </Form.Control>
-          {errors.price && <Form.Message>{errors.price.message}</Form.Message>}
-        </Form.Field>
-
-        <Form.Field name="discountPercentage">
-          <Form.Label>Discount Percentage</Form.Label>
-          <Form.Control
-            asChild
-            onChange={(e) => {
-              const value = e.target.value;
-
-              if (!value) {
-                setDiscountPercentage(0);
-                return;
-              }
-
-              if (Number.isNaN(Number(value))) {
-                return;
-              }
-
-              setDiscountPercentage(Number(value));
-            }}
-          >
-            <input
-              min={0}
-              max={100}
-              type="number"
-              {...register('discountPercentage', {
-                min: { value: 0, message: '0 이상의 할인율을 입력해주세요' },
-                max: { value: 100, message: '100 이하의 할인율을 입력해주세요' },
-              })}
-            />
-          </Form.Control>
-          {errors.discountPercentage && (
-            <Form.Message>{errors.discountPercentage.message}</Form.Message>
-          )}
-        </Form.Field>
-
-        <Form.Field name="brand">
-          <Form.Label>Brand</Form.Label>
-          <Form.Control asChild>
-            <select
-              defaultValue="weebur"
-              required
-              {...register('brand', {
-                required: '필수 선택',
-              })}
-            >
-              <optgroup label="브랜드">
-                <option value="apple">Apple</option>
-                <option value="samsung">Samsung</option>
-                <option value="weebur">Weebur</option>
-              </optgroup>
-            </select>
-          </Form.Control>
-          {errors.brand && <Form.Message>{errors.brand.message}</Form.Message>}
-        </Form.Field>
+        {formFields.map((field) => (
+          <Form.Field key={field.name} name={field.name}>
+            <Form.Label>{field.label}</Form.Label>
+            <Form.Control asChild>{renderers[field.type](field, register)}</Form.Control>
+            {errors[field.name] && <Form.Message>{errors[field.name]?.message}</Form.Message>}
+          </Form.Field>
+        ))}
 
         <Form.Submit
           onClick={() => {
@@ -142,6 +53,42 @@ const CreateProductPage = () => {
       <div>최종 가격: {price * (1 - discountPercentage / 100)}</div>
     </section>
   );
+};
+
+const renderers = {
+  text: (field: ProductFormFieldType, register: UseFormRegister<ProductType>) => (
+    <input type="text" placeholder={field.placeholder} {...register(field.name)} />
+  ),
+  number: (field: ProductFormFieldType, register: UseFormRegister<ProductType>) => (
+    <input
+      value={0}
+      type="number"
+      placeholder={field.placeholder}
+      {...register(field.name, {
+        valueAsNumber: true,
+        required: field.required,
+      })}
+    />
+  ),
+  textarea: (field: ProductFormFieldType, register: UseFormRegister<ProductType>) => (
+    <textarea placeholder={field.placeholder} {...register(field.name)} />
+  ),
+  select: (field: ProductFormFieldType, register: UseFormRegister<ProductType>) => {
+    if (field.type !== 'select') return null;
+
+    return (
+      <select defaultValue="" {...register(field.name)} required={field.required}>
+        <option value="" disabled>
+          {field.placeholder}
+        </option>
+        {field.options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    );
+  },
 };
 
 export default CreateProductPage;
