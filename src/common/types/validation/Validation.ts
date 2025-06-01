0c55bfd7f991e55726c0679c75dbfb635309e0e8
DearYuto <reasons@kakao.com>
2025-06-01 19:36:44 +0900

    chore: 코드 리팩토링을 위해 사용하지 않을 코드 제거

--- src/common/types/validation/Validation.ts
+++
@@ -1,7 +0,0 @@
-export interface Validation {
-  minLength?: { value: number; message?: string };
-  maxLength?: { value: number; message?: string };
-  min?: { value: number; message?: string };
-  max?: { value: number; message?: string };
-  pattern?: { value: RegExp; message?: string };
-}
--- src/domains/product/service/createProductService.ts
+++
@@ -1,6 +0,0 @@
-import { ProductType } from '@/domains/product/types/product';
-import { productAPI } from '@/domains/product/infra/api/productAPI';
-
-export const createProduct = async (input: ProductType) => {
-  return await productAPI.createProduct(input);
-};
