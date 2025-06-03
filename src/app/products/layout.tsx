2aec184f8d6ddbef212e44773e98ada1b16a4c06
DearYuto <reasons@kakao.com>
2025-06-03 14:58:58 +0900

    refactor: 시맨틱 태그로 변경,페이지 로딩처리 스피너 컴포넌트가 보이도록 변경

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,11 +1,19 @@
+import Navigation from '@/common/layouts/Navigation';
+import Link from 'next/link';
 import { Toaster } from 'sonner';
 
 const ProductsLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <main className="absolute left-0 top-0 w-full">
       <Toaster />
-      {children}
-    </>
+      <Navigation>
+        <button className="rounded-md bg-blue-500 px-4 py-2 text-white">
+          <Link href="/products/new">상품 생성</Link>
+        </button>
+      </Navigation>
+
+      <div className="p-6">{children}</div>
+    </main>
   );
 };
 
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,26 +1,13 @@
 'use client';
 
-import ProductList from '@/domains/product/components/list/ProductList';
+import ProductListContainer from '@/domains/product/components/list/ProductListContainer';
 import { useViewType } from '@/domains/product/hooks/useProductsViewType';
-import Link from 'next/link';
+import Spinner from '@/common/components/Spinner';
 
 export default function ProductsPage() {
   const { viewType, isLoading } = useViewType();
 
-  if (isLoading || !viewType) {
-    return <div>Loading...</div>;
-  }
+  const showSpinner = isLoading || !viewType;
 
-  return (
-    <section className="flex flex-col gap-4">
-      <nav className="flex gap-4 justify-between p-4">
-        <h2 className="text-2xl font-bold">Products</h2>
-        <button className="bg-blue-500 text-white p-2 rounded-md">
-          <Link href="/products/new">상품 생성</Link>
-        </button>
-      </nav>
-
-      <ProductList viewType={viewType} />
-    </section>
-  );
+  return <>{showSpinner ? <Spinner /> : <ProductListContainer viewType={viewType} />}</>;
 }
