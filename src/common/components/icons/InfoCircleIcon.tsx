1ec0389f9cfdd66211b40b814df63dd8e9e1f86a
DearYuto <reasons@kakao.com>
2025-06-04 06:59:32 +0900

    feat(InfoCircleIcon): 느낌표 아이콘 컴포넌트 추가

---
+++ src/common/components/icons/InfoCircleIcon.tsx
@@ -0,0 +1,19 @@
+const InfoCircleIcon = () => {
+  return (
+    <svg
+      className="h-10 w-10 text-blue-500"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 9v2m0 4h.01M21 12c0 4.97-4.03 9-9 9s-9-4.03-9-9 4.03-9 9-9 9 4.03 9 9z"
+      />
+    </svg>
+  );
+};
+
+export default InfoCircleIcon;
